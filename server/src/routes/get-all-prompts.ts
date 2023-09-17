b48ec439be7372428578566b1f82a759529439ea
GabrielBorges2000 <gabriel.vscode@gmail.com>
2023-09-17 02:50:27 -0300

    Project Finish

--- server/src/routes/get-all-prompts.ts
+++ server/src/routes/get-all-prompts.ts
@@ -3,12 +3,8 @@ import { prisma } from '../lib/prisma'
 
 export async function getAllPromptsRoute(app: FastifyInstance) {
   app.get('/prompts', async () => {
-    try {
-      const prompts = await prisma.prompt.findMany()
+    const prompts = await prisma.prompt.findMany()
 
-      return { prompts }
-    } catch (error) {
-      console.log(error)
-    }
+    return prompts
   })
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,11 @@ import { Github, Wand2 } from 'lucide-react'
 import { Separator } from './components/ui/separator'
 import { Textarea } from './components/ui/textarea'
 import { Label } from './components/ui/label'
+import { Slider } from './components/ui/slider'
+import { VideoInputForm } from './components/video-input-form'
+import { PromptSelect } from './components/prompt-select'
+import { useState } from 'react'
+import { useCompletion } from 'ai/react'
 import {
   Select,
   SelectContent,
@@ -10,84 +15,96 @@ import {
   SelectTrigger,
   SelectValue,
 } from './components/ui/select'
-import { Slider } from './components/ui/slider'
-import { VideoImputForm } from './components/video-input-form'
 
 export default function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
   return (
-    <div className="flex min-h-screen flex-col">
-      <div className="flex items-center justify-between border-b px-6 py-3">
-        <h1 className="text-2xl font-bold">Upload.AI</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolcido com 💜 no NLW sa Rockseat
+            Desenvolvido com 💜 no NLW da Rocketseat
           </span>
 
           <Separator orientation="vertical" className="h-6" />
 
           <Button variant="outline">
-            <Github className="mr-2 h-4 w-4" />
-            Github
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
           </Button>
         </div>
       </div>
 
-      <main className="flex flex-1 gap-6 p-6">
-        <div className="flex flex-1 flex-col gap-4">
-          <div className="grid flex-1 grid-rows-2 gap-4">
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
-              placeholder="Inclua o prompt para a IA..."
               className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
-              placeholder="Resultado gerado pela IA..."
-              readOnly
               className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA"
+              readOnly
+              value={completion}
             />
           </div>
 
           <p className="text-sm text-muted-foreground">
             Lembre-se: você pode utilizar a variável{' '}
-            <code className="text-violet-400">{'{transcription}'}</code> no seu
-            prompt para adicionar o conteúdo da transcrição do video
+            <code className="text-violet-400">{`{transcription}`}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
             selecionado.
           </p>
         </div>
 
-        <aside className="w-80 space-y-6 ">
-          <VideoImputForm />
+        <aside className="w-80 space-y-6">
+          <VideoInputForm onVideoUploaded={setVideoId} />
+
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um promp..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do Youtube</SelectItem>
-                  <SelectItem value="description">
-                    Descrição do YouTube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className="space-y-2">
               <Label>Modelo</Label>
-              <Select defaultValue="gpt-3.5" disabled>
+              <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="gpt-3.5">GPT 3.5-turbo 16k</SelectItem>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="block text-xs italic text-muted-foreground">
-                {' '}
-                Você podera customizar essa opção em breve
+              <span className="block text-sm text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
               </span>
             </div>
 
@@ -95,21 +112,24 @@ export default function App() {
 
             <div className="space-y-4">
               <Label>Temperatura</Label>
-
-              <Slider min={0} max={1} step={0.1} />
-
-              <span className="block text-xs italic leading-relaxed text-muted-foreground">
-                Valores mais altos geram resultados mais criativos, mas menos
-                precisos. Valores mais baixos geram resultados mais precisos,
-                mas menos criativos.
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
+              <span className="block text-sm text-muted-foreground italic leading-relaxed">
+                Valores mais altor tendem a deixar o resultado mais criativo e
+                com possíveis erros.
               </span>
             </div>
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
-              <Wand2 className="ml-2 h-4 w-4" />
+              <Wand2 className="w-4 h-4 ml-2" />
             </Button>
           </form>
         </aside>
